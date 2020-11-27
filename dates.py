4968de4abb60aef8c3b905d5f827a2df685df213
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2020-11-26 21:25:39 -0300

    script de datas

--- dates.py
+++ dates.py
@@ -3,10 +3,11 @@ import datetime
 
 array_meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
-dateFormated = pd.to_datetime(dataset['DATA_CONFIRMACAO'], format='%d/%m/%Y');
+date = pd.to_datetime(dataset['DATA_CONFIRMACAO'], format='%d/%m/%Y', utc=True);
 
-dataset['newCol_date'] = dateFormated
-dataset['newCol_mes'] = dateFormated.dt.month
-dataset['newCol_ano'] = dateFormated.dt.year
+dataset['newCol_date'] = date.apply(lambda x: x.strftime('%Y-%m-%d'));
+dataset['newCol_mes'] = date.dt.month.apply(lambda x: array_meses[x - 1]);
+dataset['newCol_ano'] = date.dt.year
 
 dataset
+    
